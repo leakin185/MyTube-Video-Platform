9e04d112afa5334a8060bf80632a17e5caca336e
leakin185 <65524912+leakin185@users.noreply.github.com>
2023-02-15 15:08:17 +0800

    added collections demonstrations

---
+++ collections/CollectionDemo.java
@@ -0,0 +1,37 @@
+package com.leajava.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class CollectionDemo {
+  public static void show() {
+    Collection<String> collection = new ArrayList<>();
+    collection.add("a");
+    collection.add("b");
+    collection.add("c");
+
+    // Add multiple items in one go
+    Collections.addAll(collection, "a", "b", "c");
+
+    var size = collection.size();
+
+    collection.remove("a");
+    var containsA = collection.contains("a");
+
+    collection.clear();
+    var isEmpty = collection.isEmpty();
+
+    Object[] objectArray = collection.toArray();
+    // convert to a string array
+    String[] stringArray = collection.toArray(new String[0]);
+
+    Collection<String> other = new ArrayList<>();
+    other.add("a");
+    other.add("b");
+    other.add("c");
+    // two different objects in memory, so false
+    System.out.println(collection == other);
+    System.out.println(collection.equals(other));
+  }
+}
